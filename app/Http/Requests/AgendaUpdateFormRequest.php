68415c676dd75575da6eccaeb56d3d3871ef4e57
jujulilia <jujuscosta07@gmail.com>
2023-11-16 13:36:25 -0300

    Atualização Agenda

--- app/Http/Requests/AgendaUpdateFormRequest.php
+++ app/Http/Requests/AgendaUpdateFormRequest.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
+use function PHPUnit\Framework\returnArgument;
+
 class AgendaRequest extends FormRequest
 {
     /**
